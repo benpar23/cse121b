6b04563fb5eae48178cb5e9f3bdfa4665cfe6787
benpar23 <benparada23@gmail.com>
2024-01-16 16:34:21 -0500

    added index.html and associated files

---
+++ scripts/w02-task.js
@@ -0,0 +1,33 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Ben Parada";
+let currentYear = 2024;
+let profilePicture = 'images/profilepicture.jpg';
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
