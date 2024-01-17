d30223fbd8152e8df2c5f9f6699ca927c162d3a7
benpar23 <benparada23@gmail.com>
2024-01-16 21:43:21 -0500

    Completed W02 assignment

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -8,24 +8,41 @@ let currentYear = 2024;
 let profilePicture = 'images/profilepicture.jpg';
 
 
-
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-
-
+const imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
 
-
-
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 
 /* Step 5 - Array */
 
+let foods = ["Flat Breaded Chicken", "Pizza Hut", "Fried Chicken"];
+
+foodElement.innerHTML = foods;
+
+let additionalFood = "Pupusas";
+
+foods.push(additionalFood);
+
+foodElement.innerHTML += `<br>${foods}`;
+
+foods.shift();
+
+foodElement.innerHTML += `<br>${foods}`;
+
+foods.pop();
+
+foodElement.innerHTML += `<br>${foods}`;
 
 
 
