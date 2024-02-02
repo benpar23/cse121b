aa8d3967f6668f8177592aa4dc0032eaa947bfcc
benpar23 <benparada23@gmail.com>
2024-02-02 09:26:24 -0500

    downloaded w04 and started work on it

---
+++ scripts/w04-task.js
@@ -0,0 +1,32 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: 'Ben Parada',
+    photo: 'images/profilepicture.jpg',
+    favoriteFoods: ["Flat Breaded Chicken", "Pizza Hut", "Fried Chicken"],  
+};
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
