a831541692b94a0d77c947b4dd302f1cde73040c
benpar23 <benparada23@gmail.com>
2024-02-07 08:25:28 -0500

    Started working on w05 task

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,13 +1,34 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
+const templesElement = document.querySelector("#temples");
 
+let templesList = [];
 
 /* async displayTemples Function */
 
+const displayTemples = (temples) => {
+    temples.forEach(function (temple) {
+        const articleElement = document.createElement("article");
+        
+        const h3Element = document.createElement("h3");
+        h3Element.innerText = temple.templeName;
+
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("src", temple.imageURL);
+        imgElement.setAttribute("alt", temple.location);
+
+        articleElement.appendChild(h3Element);
+        articleElement.appendChild(imgElement);
+
+        templesElement.appendChild(articleElement);
+    })
+}
 
 /* async getTemples Function using fetch()*/
-
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+}
 
 /* reset Function */
 
