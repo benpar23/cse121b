84235e4afe56d792e81ae970f88b1f8c31847abe
benpar23 <benparada23@gmail.com>
2024-02-09 13:32:00 -0500

    kept working on disney.js

--- scripts/disney.js
+++ scripts/disney.js
@@ -2,14 +2,32 @@ const disney = document.querySelector("#disney");
 
 let disneyList = [];
 
+const displayDisneyChar = (disneyChar) => {
+    disneyChar.forEach((character) => {
+        const articleElement = document.createElement("article");
+
+        const h3Element = document.createElement("h3");
+        h3Element.innerText = character.name;
+
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("src", character.imageUrl);
+        imgElement.setAttribute("alt", character.name);
+
+        articleElement.appendChild(h3Element);
+        articleElement.appendChild(imgElement);
+
+        disney.appendChild(articleElement);
+    })
+}
+
 const getDisneyChar = async (charName) => {
     const response = await fetch(`https://api.disneyapi.dev/character?name=${charName}`);
 
     if (response.ok){
-        const character = await response.json();
+        const characters = await response.json();
 
-        disneyList.push(character);
+        disneyList = characters.data;
     }
 
-    console.log(disneyList);
+    displayDisneyChar(disneyList);    
 }
