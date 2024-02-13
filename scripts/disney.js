81e34d877c48938994bd86a6b5d557fd43b3f130
benpar23 <benparada23@gmail.com>
2024-02-13 09:12:12 -0500

    started building out favorites list functionality in disney.js

--- scripts/disney.js
+++ scripts/disney.js
@@ -2,6 +2,8 @@ const disney = document.querySelector("#disney");
 
 let disneyList = [];
 
+let favoritesList = [];
+
 const displayDisneyChar = (disneyChar) => {
     disney.innerHTML = "";
     
@@ -15,8 +17,16 @@ const displayDisneyChar = (disneyChar) => {
         imgElement.setAttribute("src", character.imageUrl);
         imgElement.setAttribute("alt", character.name);
 
+        const buttonElement = document.createElement("button");
+        buttonElement.setAttribute("id", "favoritesButton");
+        buttonElement.textContent = "Add to favorites";
+        buttonElement.addEventListener("click", () => {
+            favoritesList.push(character);
+        })
+
         articleElement.appendChild(h3Element);
         articleElement.appendChild(imgElement);
+        articleElement.appendChild(buttonElement);
 
         disney.appendChild(articleElement);
     })
@@ -38,6 +48,34 @@ const getDisneyChar = async (charName) => {
     displayDisneyChar(disneyList);    
 }
 
+const displayFavorites = (favorites) => {
+    disney.innerHTML = "";
+
+    favorites.forEach((character, index) => {
+        const articleElement = document.createElement("article");
+
+        const h3Element = document.createElement("h3");
+        h3Element.innerText = character.name;
+
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("src", character.imageUrl);
+        imgElement.setAttribute("alt", character.name);
+
+        const buttonElement = document.createElement("button");
+        buttonElement.setAttribute("id", "removeButton");
+        buttonElement.textContent = "Remove";
+        buttonElement.addEventListener("click", () => {
+            favoritesList.splice(index, 1);
+        })
+
+        articleElement.appendChild(h3Element);
+        articleElement.appendChild(imgElement);
+        articleElement.appendChild(buttonElement);
+
+        disney.appendChild(articleElement);
+    })
+}
+
 document.getElementById("search").addEventListener("keydown", event => {
     if (event.key === "Enter") {
         let disneyCharacter = document.getElementById("search").value;
@@ -50,3 +88,5 @@ document.querySelector("#searchButton").addEventListener("click", () => {
     let disneyCharacter = document.getElementById("search").value;
     getDisneyChar(disneyCharacter);
 });
+
+document.querySelector("#favoriteListButton").addEventListener("click", (displayFavorites(favoritesList)))
